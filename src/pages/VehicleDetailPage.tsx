add922e9d339db623f015d55d9fef7d5459a71a5
Tom Antony <tomantony1997@gmail.com>
2025-06-01 19:00:16 +0530

    Updated VehicleDetailPage.tsx

---
+++ src/pages/VehicleDetailPage.tsx
@@ -0,0 +1,13 @@
+// src/pages/VehicleDetailPage.tsx
+import React from "react";
+
+const VehicleDetailPage = () => {
+  return (
+    <div>
+      <h1>Vehicle Detail Page</h1>
+      <p>This page will show detailed info about a vehicle.</p>
+    </div>
+  );
+};
+
+export default VehicleDetailPage;
